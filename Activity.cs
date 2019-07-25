d10883aad45290bb527965e22b7bd15a06503780
Harpreetgill735740 <50664337+Harpreetgill735740@users.noreply.github.com>
2019-07-25 17:53:13 -0400

    navigation bar and dashboard

---
+++ Activity.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V4.View;
+using Android.Support.V4.Widget;
+using Android.Support.V7.App;
+
+using Android.Views;
+
+using Android.App;
+using Android.Content;
+
+using Android.Widget;
+
+namespace App7
+{
+    [Activity(Label = "Activity")]
+    public class Activity : AppCompatActivity, NavigationView.IOnNavigationItemSelectedListener
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity);
+
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            //SetSupportActionBar(toolbar);
+
+
+
+            DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, Resource.String.navigation_drawer_open, Resource.String.navigation_drawer_close);
+            drawer.AddDrawerListener(toggle);
+            toggle.SyncState();
+
+            NavigationView navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            navigationView.SetNavigationItemSelectedListener(this);
+            //this.Title = " welcome Admin";
+
+        }
+
+
+
+        public override void OnBackPressed()
+        {
+            DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            if (drawer.IsDrawerOpen(GravityCompat.Start))
+            {
+                drawer.CloseDrawer(GravityCompat.Start);
+            }
+            else
+            {
+                base.OnBackPressed();
+            }
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        // public override bool OnOptionsItemSelected(IMenuItem item)
+        //{
+        //  int id = item.ItemId;
+        // if (id == Resource.Id.action_settings)
+        // {
+        //    return true;
+        // }
+
+        //  return base.OnOptionsItemSelected(item);
+        //  }
+
+
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+
+            if (id == Resource.Id.nav_sales)
+            {
+                // Handle the camera action
+            }
+            else if (id == Resource.Id.nav_product)
+            {
+
+            }
+            else if (id == Resource.Id.nav_purchase)
+            {
+
+            }
+            else if (id == Resource.Id.nav_customer)
+            {
+
+            }
+            else if (id == Resource.Id.nav_category)
+            {
+
+            }
+            else if (id == Resource.Id.nav_salesperson)
+            {
+
+            }
+            else if (id == Resource.Id.nav_password)
+            {
+
+            }
+
+            DrawerLayout drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            drawer.CloseDrawer(GravityCompat.Start);
+            return true;
+        }
+    }
+}
+
